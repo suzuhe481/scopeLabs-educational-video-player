5eff67ef9cf77c1b076616d0c54a1fa4c256add2
Hector <baseballhector@gmail.com>
2024-06-18 21:42:19 -0400

    Vite config for proxy to api

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/api": {
+        // target: import.meta.env.FETCH_URL,
+        target: "https://take-home-assessment-423502.uc.r.appspot.com/api",
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
