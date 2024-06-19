b0b4140940eaf19cd1270fd6ebdf6a33fcd77f52
Hector <baseballhector@gmail.com>
2024-06-18 21:45:41 -0400

    Function to upload a video with api call

---
+++ src/helpers/uploadVideoUtil.jsx
@@ -0,0 +1,44 @@
+const uploadVideoUtil = (event) => {
+  event.preventDefault();
+
+  const METHOD = "POST";
+  const USER_ID = import.meta.env.VITE_USER;
+
+  const TITLE = event.target[0].value;
+  const VIDEO_URL = event.target[1].value;
+  const DESCRIPTION = event.target[2].value;
+
+  const body = JSON.stringify({
+    user_id: USER_ID,
+    description: DESCRIPTION,
+    video_url: VIDEO_URL,
+    title: TITLE,
+  });
+
+  // The "/api" url is a proxy that is set up with Vite's server proxy rules in the vite.config.js file.
+  const BASE_URL = "/api";
+  const PARAMS = `/videos`;
+  const URL = `${BASE_URL}${PARAMS}`;
+
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Content-Type": "application/json",
+  };
+
+  fetch(URL, {
+    method: METHOD,
+    headers: headers,
+    body: body,
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      return json;
+    })
+    .catch((error) => {
+      return error;
+    });
+};
+
+export default uploadVideoUtil;
