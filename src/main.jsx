7428490f1b9f4a6679f64fd565a99cb954f810e9
Hector <baseballhector@gmail.com>
2024-06-22 17:30:42 -0400

    Including Navbar outside of Router

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import Router from "./Router";
+import Navbar from "./components/Navbar/Navbar";
 
 import "./reset.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Navbar />
     <Router />
   </React.StrictMode>
 );
