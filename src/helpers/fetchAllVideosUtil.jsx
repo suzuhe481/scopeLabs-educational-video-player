604c3dacfd0d0175bcfe170fdb1be142ab3c80e5
Hector <baseballhector@gmail.com>
2024-06-22 20:22:48 -0400

    Updated BASE_URL which depends on dev mode

--- src/helpers/fetchAllVideosUtil.jsx
+++ src/helpers/fetchAllVideosUtil.jsx
@@ -5,7 +5,9 @@ const fetchAllVideosUtil = () => {
   const USER = import.meta.env.VITE_USER;
 
   // The "/api" url is a proxy that is set up with Vite's server proxy rules in the vite.config.js file.
-  const BASE_URL = "/api";
+  // Will use proxy URL if in dev mode.
+  const BASE_URL =
+    import.meta.env.VITE_DEV === "true" ? "/api" : import.meta.env.PROD_URL;
   const PARAMS = `/${RESOURCE}?user_id=${USER}`;
   const URL = `${BASE_URL}${PARAMS}`;
 
