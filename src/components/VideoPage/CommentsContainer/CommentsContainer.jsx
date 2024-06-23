6ad9b14a39d1ab6587d1bc5e6da908aca0ebe0bd
Hector <baseballhector@gmail.com>
2024-06-22 23:19:23 -0400

    Importing useRef

--- src/components/VideoPage/CommentsContainer/CommentsContainer.jsx
+++ src/components/VideoPage/CommentsContainer/CommentsContainer.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
 import { DateTime } from "luxon";
 
