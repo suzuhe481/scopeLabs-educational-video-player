807effc1391eed81c0baf80f46f3c1d7f9e557ce
Hector <baseballhector@gmail.com>
2024-06-23 16:10:54 -0400

    Changed PROD_URL variable name

--- src/helpers/fetchAllVideosUtil.jsx
+++ src/helpers/fetchAllVideosUtil.jsx
@@ -7,7 +7,9 @@ const fetchAllVideosUtil = () => {
   // The "/api" url is a proxy that is set up with Vite's server proxy rules in the vite.config.js file.
   // Will use proxy URL if in dev mode.
   const BASE_URL =
-    import.meta.env.VITE_DEV === "true" ? "/api" : import.meta.env.PROD_URL;
+    import.meta.env.VITE_DEV === "true"
+      ? "/api"
+      : import.meta.env.VITE_PROD_URL;
   const PARAMS = `/${RESOURCE}?user_id=${USER}`;
   const URL = `${BASE_URL}${PARAMS}`;
 
