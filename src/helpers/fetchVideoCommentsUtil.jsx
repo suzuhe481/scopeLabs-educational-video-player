6abcd8483a1132f65ae75a742902a34d440e076b
Hector <baseballhector@gmail.com>
2024-06-22 20:25:48 -0400

    Function to fetch a video's comments with API call

---
+++ src/helpers/fetchVideoCommentsUtil.jsx
@@ -0,0 +1,33 @@
+// Fetches data from the API.
+const fetchVideoCommentsUtil = (id) => {
+  const METHOD = "GET";
+  const RESOURCE = "videos/comments";
+
+  // The "/api" url is a proxy that is set up with Vite's server proxy rules in the vite.config.js file.
+  // Will use proxy URL if in dev mode.
+  const BASE_URL =
+    import.meta.env.VITE_DEV === "true" ? "/api" : import.meta.env.PROD_URL;
+  const PARAMS = `/${RESOURCE}?video_id=${id}`;
+  const URL = `${BASE_URL}${PARAMS}`;
+
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Content-Type": "application/json",
+  };
+
+  return fetch(URL, {
+    method: METHOD,
+    headers: headers,
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      return json;
+    })
+    .catch((error) => {
+      return error;
+    });
+};
+
+export default fetchVideoCommentsUtil;
