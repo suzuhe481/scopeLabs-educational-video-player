5d72f4397479f3997c8e0bf71d4e67b50b206600
Hector <baseballhector@gmail.com>
2024-06-23 23:19:42 -0400

    Refactored to use VideoFrame component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,7 @@
 import { useState, useEffect } from "react";
 import fetchAllVideosUtil from "../../helpers/fetchAllVideosUtil";
-import { DateTime } from "luxon";
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faComment } from "@fortawesome/free-solid-svg-icons";
+import VideoFrame from "../UI/VideoFrame/VideoFrame";
 
 import styles from "./Home.module.scss";
 
@@ -15,53 +13,8 @@ const Home = () => {
   // videoData is set and is not empty.
   // Creates videoCollection element.
   if (videoData) {
-    videoCollection = videoData.map((video) => {
-      const id = video.video_url.split("?v=");
-      const video_ID = id[1];
-      const video_thumbnail =
-        video_ID === undefined
-          ? false
-          : `http://img.youtube.com/vi/${video_ID}/0.jpg`;
-
-      // Sets a thumbnail for the video if it has one.
-      const thumbnailStyles = !video_thumbnail
-        ? {}
-        : {
-            backgroundImage: `url(${video_thumbnail})`,
-            backgroundPosition: "center",
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat",
-          };
-
-      // Converting date to user friendly string with Luxon.
-      const video_created_date = new DateTime(video.created_at).toLocaleString(
-        DateTime.DATE_MED
-      );
-
-      return (
-        <div className={`${styles["video-container"]}`} key={video.id}>
-          <a
-            href={`video/${video.id}`}
-            className={`${styles["remove-link-style"]}`}
-          >
-            <div className={styles.thumbnail} style={thumbnailStyles}></div>
-            <div className={`${styles["info-container"]}`}>
-              <div className={styles.title}>{video.title}</div>
-              <div>
-                {video.user_id} • {video_created_date}
-              </div>
-              <FontAwesomeIcon
-                icon={faComment}
-                className={`${styles["comment-icon"]}`}
-              />
-              <div className={`${styles["comment-text"]}`}>
-                {video.num_comments}{" "}
-                {video.num_comments === 1 ? "comment" : "comments"}
-              </div>
-            </div>
-          </a>
-        </div>
-      );
+    videoCollection = videoData.map((video, index) => {
+      return <VideoFrame video={video} key={index} />;
     });
   }
 
