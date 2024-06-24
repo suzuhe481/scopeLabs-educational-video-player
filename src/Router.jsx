1c750d84493364fb1dea46e777a41b00c82809ef
Hector <baseballhector@gmail.com>
2024-06-24 15:22:01 -0400

    Including SearchUserPage in Router

--- src/Router.jsx
+++ src/Router.jsx
@@ -4,6 +4,7 @@ import Home from "./components/Home/Home";
 import UploadVideoPage from "./components/UploadVideoPage/UploadVideoPage";
 import VideoPage from "./components/VideoPage/VideoPage";
 import EditVideoPage from "./components/EditVideoPage/EditVideoPage";
+import SearchUserPage from "./components/SearchUserPage/SearchUserPage";
 import ErrorPage from "./components/ErrorPage/ErrorPage";
 
 const Router = () => {
@@ -23,6 +24,10 @@ const Router = () => {
     {
       path: "/edit/video/:id",
       element: <EditVideoPage />,
+    },
+    {
+      path: "/search/:id",
+      element: <SearchUserPage />,
     },
     {
       path: "*",
